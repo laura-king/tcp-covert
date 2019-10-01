c3923133cf4218e1600b9a69c57e583072c9dba4
laura_king <lking1106@gmail.com>
2019-10-01 16:27:04 -0400

    import and char change

--- ID_IP_CC.py
+++ ID_IP_CC.py
@@ -1,4 +1,6 @@
-from scapy.all import IP
+from scapy.all import *
+from scapy.layers.inet import IP
+
 
 out_ip = IP()
 
@@ -16,13 +18,14 @@ for letter in msg:
 	# send packet! 
 	send(out_ip)
 
+
 ##### DECODING
 for inc_packet in sniff(iface=""):
 	# if this packet is for me 
 	if inc_packet.src == out_ip.dst:
 		# check ID field and decode 
 		encoded_letter_out = inc_packet.id
-		decoded_letter = char( encoded_letter_out / 256 )
+		decoded_letter = chr( encoded_letter_out / 256 )
 
 
 		
