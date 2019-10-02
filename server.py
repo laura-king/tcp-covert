c573466de41c28eb4308eb96d18b9fce40e6ff7c
kris10k8 <kmt5215@rit.edu>
2019-10-02 16:47:56 -0400

    updated server

--- server.py
+++ server.py
@@ -3,15 +3,22 @@ from scapy.all import IP
 
 eom = "!" 
 enc_let = ""
+global msg
 msg = "" 
 
 def sniff_packet(intface):
-	sniff(iface=intface, prn=process_packet, store=False)
+	sniff(iface=intface, prn=process_packet, store=False, count=6)
 
 def process_packet(packet):
+	global msg
+	if packet.src == '127.0.0.1':
+		return
 	enc_let = int(packet.id)
 	dec_let = chr(enc_let // 256)
-	msg += dec_let
+	if not msg:	
+		msg += dec_let
+	elif dec_let != msg[-1]:
+		msg += dec_let
 
 sniff_packet("lo")
 print(msg)
